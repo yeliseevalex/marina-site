a825c97577ffcc34940d01dd537f9ad1d3b147e2
Александр Елисеев <yeliseevalex20@gmail.com>
2026-01-02 03:29:54 +0400

    Stop tracking ignored folders

--- not-for-git/reviewPopup.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.review-overlay').forEach(overlay => {
-    overlay.addEventListener('click', () => {
-        const parent = overlay.closest('.slide-left');
-
-        if (overlay.classList.contains('bottom-overlay')) {
-            parent.classList.toggle('bottom-open');
-        }
-    });
-});
